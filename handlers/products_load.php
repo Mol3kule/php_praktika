2bf2cc4e43f9526ca3e9c5702d244de9f2fe4aca
Juliukazz <julexksk@gmail.com>
2022-12-05 11:04:36 +0200

    Create products_load.php

---
+++ handlers/products_load.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once './database.php';
+
+function LoadProductsPaged($category) {
+    if (isset($_GET['page_no']) && $_GET['page_no'] != "") {
+        $page_no = $_GET['page_no'];
+    } else {
+        $page_no = 1;
+    }
+
+    $total_records_per_page = 10;
+    $offset = ($page_no - 1) * $total_records_per_page;
+    $previous_page = $page_no - 1;
+    $next_page = $page_no + 1;
+    $adjacents = "2";
+
+    $result_count = $connection->prepare("SELECT COUNT(*) AS total_records FROM `products`");
+    $result_count->execute();
+    $total_records = $result_count->fetchAll();
+    foreach ($total_records as $record_count) {
+        $total_records = $record_count['total_records'];
+    }
+    $total_no_of_pages = ceil($total_records / $total_records_per_page);
+    $second_last = $total_no_of_pages - 1; // total pages minus 1
+    // END OF PAGINATION
+
+    $statement = $connection->prepare("SELECT * FROM products LIMIT $offset, $total_records_per_page");
+    $statement->execute();
+    $result = $statement->fetchAll();
+}
